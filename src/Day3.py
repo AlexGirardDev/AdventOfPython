950836d9a0bbb5249c2e8d80c7ba0ff144eaaef3
Alex Girard <me@alexgirard.ca>
2019-11-23 23:25:33 -0500

    day 3 pt 2

--- src/Day3.py
+++ src/Day3.py
@@ -2,6 +2,29 @@ f = list(open("input/day3.txt", "r"))
 
 
 def day3_1(arg):
+    grid = build_grid(arg)
+    result = 0
+    for x in grid:
+        for y in x:
+            if len(y) > 1:
+                result +=1
+
+    return result
+
+
+def day3_2(arg):
+    grid = build_grid(arg)
+    overlapped = set()
+    clean = set()
+    for x in grid:
+        for y in x:
+            if (len(y) > 1):
+                overlapped.update(y)
+            else:
+                clean.update(y)
+    return [x for x in clean if x not in overlapped][0]
+
+def build_grid(arg):
     grid_size = 1000
     grid = [[[] for y in range(grid_size)] for x in range(grid_size)]
 
@@ -12,18 +35,10 @@ def day3_1(arg):
         y_pos = int(a.split("@")[1].split(",")[1].split(":")[0])
         w_grid = int(a.split(":")[1].split("x")[0])
         h_grid = int(a.split("x")[1])
-        print(id)
         for x in range(w_grid):
             for y in range(h_grid):
                 grid[x_pos + x][y_pos + y].append(id)
-        
-    result = 0
-    for x in grid:
-        for y in x:
-            if len(y) > 1:
-                result +=1
-
-    return result
-
+    return grid
 
 print(day3_1(f))
+print(day3_2(f))
