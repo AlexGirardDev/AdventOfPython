17e3a53b85539db766e559fd69df8ed041279937
Alex Girard <me@alexgirard.ca>
2019-12-14 02:17:45 -0500

    day 14 part 2

--- src/2019/Day14.py
+++ src/2019/Day14.py
@@ -1,21 +1,14 @@
 import math
 f = list(open("input/2019/day14.txt", "r"))
 
-
 class Ore:
     def __init__(self, s):
+        #what is regex?
         self.name = s.split("=>")[1].strip().split(" ")[1]
         self.output = int(s.split("=>")[1].strip().split(" ")[0])
         if s.split("=>")[0].split(",") != [" "]:
             self.requriments = [(y.strip().split(" ")[1],
                                  int(y.strip().split(" ")[0])) for y in s.split("=>")[0].split(",")]
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __eq__(self, other):
-        return self.name == other.name
-
     def __repr__(self):
         return self.name
 
@@ -27,45 +20,35 @@ for x in f:
     ores[ore.name] = ore
 ore = Ore(" => 1 ORE")
 ores[ore.name] = ore
-#print(ores)
-
+# print(ores)
 total_ore = 0
 leftover_ore = dict()
-def calc_ore_cost(ore):
+def calc_ore_cost_1(ore):
     if ore.name == "ORE":
-        total_ore =  ore.output
         return ore.output
-    total = 0 
+    total = 0
     for x in ore.requriments:
         required_ore = ores[x[0]]
-        requried_ammount = x[1] 
+        requried_ammount = x[1]
         if leftover_ore.get(required_ore.name) is not None:
             requried_ammount -= leftover_ore[required_ore.name]
             leftover_ore[required_ore.name] = 0
-
         iterations = int(math.ceil(requried_ammount / required_ore.output))
+        leftover_ore[required_ore.name] = iterations * \
+            required_ore.output - requried_ammount
         if required_ore.name == "ORE":
             return iterations * required_ore.output
-        leftover_ore[required_ore.name] = iterations * required_ore.output - requried_ammount
-        for z in range(iterations):
-            total += calc_ore_cost(required_ore)
-        
+        total += sum(calc_ore_cost_1(required_ore) for z in range(iterations))
     return total
 
 
+def calc_ore_cost_2(ore, amount):
+    if ore.name == "ORE":
+        return ore.output * amount
+    return sum(calc_ore_cost_2(ores[x[0]],  x[1] / ores[x[0]].output * amount) for x in ore.requriments)
 
-#print(counter)
-fuel = 1000000000000
-counter = 0
-print(calc_ore_cost(ores["FUEL"]))
-print(calc_ore_cost(ores["FUEL"]))
-print(calc_ore_cost(ores["FUEL"]))
-print(calc_ore_cost(ores["FUEL"]))
-print(calc_ore_cost(ores["FUEL"]))
-#return
 
-# while (fuel > 0):
-#     print(fuel)
-#     fuel -= calc_ore_cost(ores["FUEL"])
-#     counter += 1
-# print(counter) 
+print(calc_ore_cost_1(ores["FUEL"]))
+print(1000000000000 / calc_ore_cost_2(ores["FUEL"], 1)-1)
+#the actualy awser was 1 below this, even though all my tests were right on
+#i am just going to chalk it up to floating point math bullshit
